1ef6fc5d7be90d91e655fda2ff682f7c903548dc
Maximilian <scientificresearch309@gmail.com>
2023-09-08 22:46:10 +0200

    Schemas

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const mongoose = require("mongoose");
+const { boolean } = require("webidl-conversions");
 
 mongoose
   .connect("mongodb://127.0.0.1/playground")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.log("Could not connect to MongoDB...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  isPublished: Boolean,
+});
