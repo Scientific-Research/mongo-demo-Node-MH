b71b35e2e1957a7e699b4fdd3acef22b6debe50b
Maximilian <scientificresearch309@gmail.com>
2023-09-09 23:21:15 +0200

    .or([{ author: "Mosh" }, { isPublished: true }])     .and([])

--- index.js
+++ index.js
@@ -34,10 +34,12 @@ const createCourse = async () => {
 // createCourse();
 
 const getCourses = async () => {
-  const courses = await Course
-    //   .find({ author: "Mosh", isPublished: true })
+  const courses = await Course.find()
+    .or([{ author: "Mosh" }, { isPublished: true }])
+    .and([])
+    //.find({ author: "Mosh", isPublished: true })
     // .find({ price: { $gte: 10, lte: 20 } })
-    .find({ price: { $in: [10, 15, 20] } })
+    // .find({ price: { $in: [10, 15, 20] } })
     .limit(10)
     .sort({ name: -1 }) // 1: Ascending -1: Descending
     .select({ name: 1, tags: 1, author: 1 });
