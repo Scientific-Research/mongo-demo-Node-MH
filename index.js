7e426a27afffe80676363f41a22124bf7360d40b
Maximilian <scientificresearch309@gmail.com>
2023-09-09 23:15:10 +0200

    .find({ price: { $gte: 10, lte: 20 } })

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const createCourse = async () => {
 const getCourses = async () => {
   const courses = await Course
     //   .find({ author: "Mosh", isPublished: true })
-    .find({ price: { $gte: 10 } })
+    .find({ price: { $gte: 10, lte: 20 } })
     .limit(10)
     .sort({ name: -1 }) // 1: Ascending -1: Descending
     .select({ name: 1, tags: 1, author: 1 });
