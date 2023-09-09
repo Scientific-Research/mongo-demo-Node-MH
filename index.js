d77288079601f3481eb11f1266688e06ce5b9e75
Maximilian <scientificresearch309@gmail.com>
2023-09-09 23:13:04 +0200

    price for greater than 10

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ const createCourse = async () => {
 // createCourse();
 
 const getCourses = async () => {
-  const courses = await Course.find({ author: "Mosh", isPublished: true })
+  const courses = await Course
+    //   .find({ author: "Mosh", isPublished: true })
+    .find({ price: 10 })
     .limit(10)
     .sort({ name: -1 }) // 1: Ascending -1: Descending
     .select({ name: 1, tags: 1, author: 1 });
